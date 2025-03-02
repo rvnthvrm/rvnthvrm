27f06fd00c6db284758d246a6ca34a6d9fe09826
Revanth <rvnthvrm@gmail.com>
2025-03-02 08:15:01 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector('.hamburger').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
